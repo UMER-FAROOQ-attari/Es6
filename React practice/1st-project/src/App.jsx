19b538701239e2c0a3cffd8a930725fe6da605db
UMER-FAROOQ-attari <khadeemattar@gmail.com>
2025-07-28 16:20:58 +0500

    complete

--- React practice/1st-project/src/App.jsx
+++ React practice/1st-project/src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React,{ useState , useEffect } from 'react'
 import './App.css'
+import "tailwindcss"
+import './componet/Nav'
+import Nav from './componet/Nav'
+import BlurText from './componet'
+import { Carousel } from 'flowbite-react'
 const App = () => {
+  const [time, settime] = useState("")
+    const clock  =  ()=> {
+    let date = new Date();
+      let hh = date.getHours();
+      let mm = date.getMinutes();
+      let ss = date.getSeconds();
+      let ft = `${hh}:${mm}:${ss}`
+      
+      return settime(ft) 
+    }
+
+ useEffect(() => {
+    const interval = setInterval(clock, 1000);  // Update time every second
+    return () => clearInterval(interval);
+  }, []);    
   return (
  <>
- <h1>12:00:00</h1>
+ <Nav />
+ <h1 className='text-[30px] bg-amber-300 text-emerald-700'>{time}</h1>
  </>
   )
 }
